be77a6e1371884f22f8121ed86c735a837981ac0
Daniel Earwicker <dan@earwicker.com>
2015-03-17 23:39:53 +0000

    Render on props change

--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ export class Komponent<P, S> extends React.Component<P, S> {
         rendered?: React.ReactElement<any>;
         listener?: Kor.Computed<void>;
         subscription?: Kor.Subscription;
+        renderSoon?: Kor.Observable<void>;
     };
 
     private _koInternals() {
@@ -18,10 +19,19 @@ export class Komponent<P, S> extends React.Component<P, S> {
         return this._koCachedInternals;
     }
 
+    componentWillReceiveProps(nextProps: P) {
+        var internals = this._koInternals();
+        if (internals.renderSoon) {
+            internals.renderSoon.notifySubscribers();
+        }
+    }
+
     componentDidMount() {
         var internals = this._koInternals();
         if (!internals.listener) {
+            internals.renderSoon = kor.observable<void>();
             internals.listener = kor.pureComputed(() => {
+                internals.renderSoon();
                 internals.rendered = this.renderKor();
                 this.forceUpdate();
             }).extend({ throttle: 1 });
